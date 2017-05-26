b8d3d74c29d299caaa83f627742e7cef1f8eef69
Torben <mail@sauce-code.com>
2017-05-26 17:21:02 +0200

    change title

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -122,7 +122,7 @@ public class Gui extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Chess Game");
+		primaryStage.setTitle("Neo Chess");
 
 		initializeGrid();
 
